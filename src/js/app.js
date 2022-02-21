21371c5a3d185344dc424c9cea6f59c711ec2b20
Amantay <amantay.sv@gmail.com>
2022-02-21 19:23:26 +0600

    modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,53 @@
-//get modal element
-var modal = document.getElementById('modal');
-var modalBtn = document.getElementById('modalBtn');
-console.log(modalBtn);
-//get close btn
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
+// modal
+const loginModal = document.getElementById('modal-login')
+const registrationModal = document.getElementById('modal-registration')
+const thanksModal = document.getElementById('modal-thanks')
 
+const modalBtn = document.getElementById('modalBtn')
+const modalGoToLoginBtn = document.getElementById('modal-goToLoginBtn')
+const modalGoToRegistrationBtn = document.getElementById(
+  'modal-goToRegistrationBtn'
+)
+const closeBtns = document.getElementsByClassName('modal-close-btn')
+
+modalBtn.addEventListener('click', () => {
+  addClass(loginModal)
+})
+
+for (const btn of closeBtns) {
+  btn.addEventListener('click', () => {
+    if (loginModal.classList.contains('active')) {
+      loginModal.classList.remove('active')
+    }
+  })
+}
+
+modalGoToLoginBtn.addEventListener('click', () => {
+  removeClass(registrationModal)
+  addClass(loginModal)
+})
+
+modalGoToRegistrationBtn.addEventListener('click', () => {
+  removeClass(loginModal)
+  addClass(registrationModal)
+})
 //to open modal
-modalBtn.addEventListener('click', openModal);
 
-//to close modal
-closeBtn.addEventListener('click', closeModal);
-
-window.addEventListener('click', outsideClick);
+window.addEventListener('click', outsideClick)
 
 //func to open modal
-function openModal(){
-    modal.style.display = 'block';
+function addClass(selector) {
+  selector.classList.add('active')
 }
 
 //func to close modal
-function closeModal(){
-    modal.style.display = 'none';
+function removeClass(selector) {
+  selector.classList.remove('active')
 }
 
 //func to close modal if outside click
-function outsideClick(e){
-    if(e.target == modal){
-        modal.style.display = 'none';
-    }
+function outsideClick(e) {
+  if (e.target == loginModal) {
+    loginModal.style.display = 'none'
+  }
 }
